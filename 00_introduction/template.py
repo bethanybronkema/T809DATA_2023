d0e0be54526a0a041bd039777b7798decc09f188
bethanybronkema <bethany23@ru.is>
2023-08-24 15:15:15 +0000

    work 1

--- 00_introduction/template.py
+++ 00_introduction/template.py
@@ -13,7 +13,7 @@ def plot_normal(sigma: float, mu:float, x_start: float, x_end: float):
     plt.plot(x,normal(x, sigma, mu))
 
 def _plot_three_normals():
-    # Part 1.2
+    
 
 def normal_mixture(x: np.ndarray, sigmas: list, mus: list, weights: list):
     # Part 2.1
