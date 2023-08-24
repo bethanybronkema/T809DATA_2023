1ef39d70ab30f661ab31c499f5d72109c286980c
bethanybronkema <bethany23@ru.is>
2023-08-24 15:15:00 +0000

    work 1

--- 00_introduction/template.py
+++ 00_introduction/template.py
@@ -3,9 +3,14 @@ import matplotlib.pyplot as plt
 
 def normal(x: np.ndarray, sigma: float, mu: float) -> np.ndarray:
     d1 = 2*np.power(sigma,2)
+    n1 = -np.power(x-mu,2)
+    d2 = np.sqrt(2*np.pi*np.power(sigma,2))
+    p = (1/d2)*np.exp(n1/d1)
+    return p
 
 def plot_normal(sigma: float, mu:float, x_start: float, x_end: float):
-    # Part 1.2
+    x = np.linspace(x_start, x_end, num=500)
+    plt.plot(x,normal(x, sigma, mu))
 
 def _plot_three_normals():
     # Part 1.2
@@ -24,3 +29,4 @@ def _plot_mixture_and_samples():
 
 if __name__ == '__main__':
     # select your function to test here and do `python3 template.py`
+'''
