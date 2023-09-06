c706495bb5f90a6ec349871211ffb38fa4cb72ed
bethanybronkema <bethany23@ru.is>
2023-09-06 13:59:03 +0000

    workfinal1

--- 03_sequential_estimation/template.py
+++ 03_sequential_estimation/template.py
@@ -36,7 +36,7 @@ def update_sequence_mean(
     '''
     Performs the mean sequence estimation update
     '''
-    mu = mu + (1/(n+1))*(x-mu)
+    mu = mu + (1/(n + 1)) * (x - mu)
     return mu
 '''
 mean = np.mean(X, 0)
@@ -48,22 +48,29 @@ def _plot_sequence_estimate():
     data = gen_data(100, 3, np.array([0, 0, 0]), 1)
     estimates = [np.array([0, 0, 0])]
     for i in range(data.shape[0]):
-        estimates = np.append(estimates, [update_sequence_mean(estimates[i], data[i], i+1)], axis = 0)
-    plt.plot([e[0] for e in estimates], label='First dimension')
-    plt.plot([e[1] for e in estimates], label='Second dimension')
-    plt.plot([e[2] for e in estimates], label='Third dimension')
-    plt.legend(loc='upper center')
-    plt.show()
-
-_plot_sequence_estimate()
+        estimates = np.append(estimates, [update_sequence_mean(estimates[i], data[i], i)], axis = 0)
+    #plt.plot([e[0] for e in estimates], label='First dimension')
+    #plt.plot([e[1] for e in estimates], label='Second dimension')
+    #plt.plot([e[2] for e in estimates], label='Third dimension')
+    #plt.legend(loc='upper center')
+    #plt.show()
+    return estimates
 
 def _square_error(y, y_hat):
-    ...
+    sqr_err = np.square(y-y_hat)
+    return np.mean(sqr_err)
 
 
 def _plot_mean_square_error():
-    ...
+    data = gen_data(100, 3, np.array([0, 0, 0]), 1)
+    error = np.zeros(len(data))
+    estimates = _plot_sequence_estimate()
+    for i in range(len(data)):
+        error[i] = _square_error(np.mean(data), estimates[i+1,:])
+    plt.plot(error)
+    plt.show()
 
+_plot_mean_square_error()
 
 # Naive solution to the independent question.
 
